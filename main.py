3d91582ad1baaf64c4c5d7edc234b308b4ec62dc
Tejaswi Polisetty <52691780+Tejaswi1120@users.noreply.github.com>
2022-01-27 22:13:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of {0} and {1} is {2}".format(num1, num2, sum))
